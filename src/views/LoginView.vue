f7b377f34c071e8d3d783221f3925b064089e658
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2023-10-10 00:38:06 +0200

    Enabled logging in

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,46 @@
 <template>
-    <form>
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <form @submit.prevent="submit">
+        <h1 class="h3 mb-3 fw-normal text-center">Please sign in</h1>
 
-        <div class="form-floating">
-            <input type="name" class="form-control" placeholder="Name">
-            <label for="floatingInput">Name</label>
-        </div>
-        <div class="form-floating">
-            <input type="password" class="form-control" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-        </div>
+        <input v-model="data.name" type="text" class="form-control" placeholder="Name" required>
+        <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
 
         <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
     </form>
 </template>
 
 <script lang="ts">
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
 export default {
-  name: 'LoginView'
+  setup () {
+    const data = reactive({
+        name: '',
+        password: ''
+    })
+
+    const router = useRouter();
+
+    const submit = async () => {
+        const response = await fetch('http://localhost:7070/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+            body: JSON.stringify(data)
+        })
+
+        if (!response.ok) {
+            console.error('Failed to login.');
+        } else {
+            await router.push('/');
+        }
+    }
+
+    return {
+        data,
+        submit
+    }
+  }
 }
 </script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,16 +1,11 @@
 <template>
     <form @submit.prevent="submit">
-        <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+        <h1 class="h3 mb-3 fw-normal text-center">Please sign up</h1>
 
-        <div class="form-floating">
-            <input v-model="data.name" type="text" class="form-control" placeholder="Name">
-        </div>
-        <div class="form-floating">
-            <input v-model="data.password" type="password" class="form-control" placeholder="Password">
-        </div>
+        <input v-model="data.name" type="text" class="form-control" placeholder="Name" required>
+        <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
 
         <button class="btn btn-primary w-100 py-2" type="submit">Submit</button>
-        <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
     </form>
 </template>
 
