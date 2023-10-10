76ac66c07fdc3bb6a9f940c54884a78ec524ff96
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2023-10-10 10:26:33 +0200

    Added almost working authentication + logout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
         <div class="container-fluid">
             <router-link to="/" class="navbar-brand" href="#">Home</router-link>
-            <div>
+            <div v-if="!auth">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
                         <router-link to="/register" class="nav-link active" aria-current="page" href="#">Register</router-link>
@@ -12,13 +12,44 @@
                     </li>
                 </ul>
             </div>
+            <div v-if="auth">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <router-link to="/login" class="nav-link active" aria-current="page" href="#" @click="logout">Logout</router-link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </template>
 
 <script lang="ts">
+import { watch, ref } from 'vue';
+import { useStore } from 'vuex';
+
 
 export default {
-  name: 'NavBar'
+  setup() {
+    const store = useStore();
+
+    const auth = ref(store.state.authenticated);
+
+    watch(() => store.state.authenticated, (newValue) => {
+      auth.value = newValue;
+    });
+
+    const logout = async () => {
+        await fetch('http://localhost:7070/api/logout', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+        });
+    }
+
+    return {
+        auth,
+        logout
+    }
+  }
 }
 </script>
