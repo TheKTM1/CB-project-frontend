50518fb56c661773ae26af62cc65b4251622f4ff
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2023-11-27 18:52:05 +0100

    Enabled ReCaptcha

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
+import { VueReCaptcha } from 'vue-recaptcha-v3' 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(VueReCaptcha, {
+        siteKey: '6LdNQhwpAAAAAI9JgdhW5H9or5hyIoxbBoCEioCO',
+        loaderOptions: {
+            useRecaptchaNet: true
+        }
+    })
+    .mount('#app')
